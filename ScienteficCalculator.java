3a4ee568bfa8162304c568144e1e3fde775bbf4e
Mohammed Al-Minji <mohaff990@gmail.com>
2025-07-20 14:33:19 +0400

    Starting File and add main methods

---
+++ ScienteficCalculator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class ScienteficCalculator {
+    public static void main(String args[]){
+        Scanner SC = new Scanner(System.in);
+        while(true){
+            int option = SC.nextInt();
+            switch (option){
+                case 1:
+
+            }
+        }
+    }
+
+
+}
